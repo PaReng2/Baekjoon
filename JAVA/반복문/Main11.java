183186c30c56a60a4fef28a07ac0b64249e558c6
PaReng2 <128889917+PaReng2@users.noreply.github.com>
2023-05-31 18:11:47 +0900

    Add files via upload

---
+++ JAVA/반복문/Main11.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count = sc.nextInt();
+        for(int loop = 1; loop<=count; loop++)
+        {
+            int st = sc.nextInt();
+            int nd = sc.nextInt();
+            int sum = st+nd;
+            System.out.println("Case #"+loop+": "+sum);
+        }
+    }
+}
+//11021
