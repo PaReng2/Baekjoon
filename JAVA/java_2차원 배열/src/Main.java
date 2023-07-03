beacc2e6e78ac3be46c53073b81e342461d9e114
PaReng2 <128889917+PaReng2@users.noreply.github.com>
2023-07-03 15:08:23 +0900

    Add files via upload

---
+++ JAVA/java_2차원 배열/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int[][] lineA = new int[N][M];
+        int[][] lineB = new int[N][M];
+
+    }
+}
---
+++ JAVA/java_문자열/src/Main.java
@@ -0,0 +1,17 @@
+package src;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        int num = sc.nextInt();
+        
+        System.out.println(input.charAt(num-1));
+
+
+
+
+
+    }
+}
