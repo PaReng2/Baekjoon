5cbb6e2abfca170c2809f0b4df6f8e718a735bd2
PaReng2 <128889917+PaReng2@users.noreply.github.com>
2023-07-04 19:20:54 +0900

    Add files via upload

---
+++ JAVA/java_문자열/src/Main03.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char Input = sc.next().charAt(0);
+        int output = (int)Input;
+        System.out.println(output);
+    }
+}
